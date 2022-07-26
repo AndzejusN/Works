9742826c3ff9d81dc5d49d3049ea2cb0a958734b
Andrew <90209255+AndzejusN@users.noreply.github.com>
2022-07-26 13:56:14 +0300

    Path change / isset -> if  / always visible scroll

--- IV/src/public/index.php
+++ IV/src/public/index.php
@@ -16,7 +16,7 @@ require_once('main.php');
 <header class="py-3 px-5">
     <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
         <a href="index.php" class="d-flex align-items-center text-dark text-decoration-none">
-            <span class="fs-4 font-hand"><h2><img src="/files/img/sos.png" alt="S.erver O.rder S.ystem"></h2></span>
+            <span class="fs-4 font-hand"><h2><img src="<?php echo '/files/img/sos.png'; ?>" alt="S.erver O.rder S.ystem"></h2></span>
         </a>
         <nav class="d-inline-flex mt-2 mt-md-0 ms-auto">
             <a href="index.php" class="me-3 py-2 text-dark text-decoration-none active"><h5>Main page</h5>
@@ -26,7 +26,6 @@ require_once('main.php');
         </nav>
     </div>
 </header>
-
 <main class="py-3 px-5">
     <div class="card text-center">
         <div class="card-header text-muted">
@@ -37,7 +36,7 @@ require_once('main.php');
     </div>
 
 	<?php
-	if (isset($dataDecoded)) {
+	if ($dataDecoded) {
 		foreach ($dataDecoded['categories'] as $data) {
 			?>
             <form action="php/add.php" method="post" class="py-1">
--- IV/src/public/list.php
+++ IV/src/public/list.php
@@ -19,7 +19,7 @@ require_once('php/read.php');
 <header class="py-3 px-5">
     <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
         <a href="index.php" class="d-flex align-items-center text-dark text-decoration-none">
-            <span class="fs-4 font-hand"><h2><img src="/files/img/sos.png" alt="S.erver O.rder S.ystem"></h2></span>
+            <span class="fs-4 font-hand"><h2><img src="<?php echo '/files/img/sos.png'; ?>" alt="S.erver O.rder S.ystem"></h2></span>
         </a>
         <nav class="d-inline-flex mt-2 mt-md-0 ms-auto">
             <a href="index.php" class="me-3 py-2 text-dark text-decoration-none"><h5>Main page</h5>
@@ -41,35 +41,39 @@ require_once('php/read.php');
     </div>
 
 	<?php
-	if (isset($orders)){
-	$count = 0;
-	foreach ($orders as $order) {
-		$count++;
-		?>
-        <form action="php/delete.php" method="post" class="py-1">
-            <input type="hidden" name="id" value="<?php echo htmlspecialchars($order['id']) ?>">
-            <div class="card text-center">
-                <div class="card-body">
-                    <h5 class="card-title">Name: <?php echo htmlspecialchars($order['name']) ?></h5>
-                    <p class="card-text">Id: <?php echo htmlspecialchars($order['id']) ?></p>
-                    <p class="card-text">Slug: <?php echo htmlspecialchars($order['slug']) ?></p>
-                    <button type="submit" class="btn btn-outline-danger">Delete</button>
+	if (isset($orders)) {
+		$count = 0;
+		foreach ($orders as $order) {
+			$count++;
+			?>
+            <form action="php/delete.php" method="post" class="py-1">
+                <input type="hidden" name="id" value="<?php echo htmlspecialchars($order['id']) ?>">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h5 class="card-title">Name: <?php echo htmlspecialchars($order['name']) ?></h5>
+                        <p class="card-text">Id: <?php echo htmlspecialchars($order['id']) ?></p>
+                        <p class="card-text">Slug: <?php echo htmlspecialchars($order['slug']) ?></p>
+                        <button type="submit" class="btn btn-outline-danger">Delete</button>
+                    </div>
+                    <div class="card-footer text-muted">
+                        IV solutions
+                    </div>
                 </div>
-                <div class="card-footer text-muted">
-                    IV solutions
-                </div>
-            </div>
-        </form>
-	<?php } ?>
+            </form>
+			<?php
+		}
+	}
+	if ($count == 0) {
+		echo '<h4 class="text-center">No orders was made</h4>';
+	}
+	?>
 </main>
 
 <footer class="py-3 px-5">
     <div class="d-flex flex-column flex-fill align-items-end justify-content-center pb-3 mb-4 border-top">
         <div class="card text-center alert alert-dark">
             <h5><b> Totally placed orders: <?php echo $count;
-					} else {
-						echo '<h4 class="text-center">No orders was made</h4>';
-					} ?> </b></h5>
+					?> </b></h5>
         </div>
     </div>
 </footer>
